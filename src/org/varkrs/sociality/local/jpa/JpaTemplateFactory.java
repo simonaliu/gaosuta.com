261cbc8f40ad4ad2c5d8d1139aa12f9233449b14
lenovo <1265149506@qq.com>
2012-01-12 19:15:29 +0800

    搞完了吗?

--- src/org/varkrs/sociality/local/jpa/JpaTemplateFactory.java
+++ src/org/varkrs/sociality/local/jpa/JpaTemplateFactory.java
@@ -1,11 +1,14 @@
 package org.varkrs.sociality.local.jpa;
 
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 import org.springframework.orm.jpa.JpaTemplate;
 
 public class JpaTemplateFactory {
+	private EMFFactory emfFactory;
+	
+	public JpaTemplateFactory() {}
+	
 	public JpaTemplate getInatance() {
 		EntityManagerFactory emf = getEMF();
 		JpaTemplate template = new JpaTemplate(emf);
@@ -13,6 +16,11 @@ public class JpaTemplateFactory {
 	}
 
 	private EntityManagerFactory getEMF() {
-		return Persistence.createEntityManagerFactory("local");
+		return emfFactory.getEMF();
 	}
+	
+	public void setEMFFactory(EMFFactory factory) {
+		this.emfFactory = factory;
+	}
+	
 }
