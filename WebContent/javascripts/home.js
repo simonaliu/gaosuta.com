59627390c095c3a53086491f881b869c8c9fb3d2
rightgenius <rightgenius@gmail.com>
2012-01-13 14:17:28 +0800

    now we have dust.js to template

--- WebContent/javascripts/home.js
+++ WebContent/javascripts/home.js
@@ -22,3 +22,21 @@
 			   });
 		 
 		  });
+	  
+//template of page
+(function(){
+	//html template in string
+	var record = '<div>hello world {name}</div>';
+	//compile template into object source, template source name is 'record'
+	var r_c = dust.compile(record, 'record');
+	//load the source into the library
+	dust.loadSource(r_c);
+})();
+
+$(function(){
+	//render the template named record
+	dust.render('record', {name: 'rightgenius'}, function(err, out){
+		console.log(out);
+		$("#left_content").append(out);
+	});
+});
